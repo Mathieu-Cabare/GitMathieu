4da8e7ebc327a5fa6f88081d8fbc5d8d2b472f80
mathieu.cabare@gmail.com <mathieu.cabare@gmail.com>
2020-05-05 16:02:55 +0200

    demande

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+
+
+<div class="container">
+
+    <div class="bg bg-success rounded">
+        <h1 class="h1 text-center">Git Mathieu</h1>
+            <div class="col-md-6 text-center mx-auto text-white">
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, repudiandae error. Tenetur recusandae excepturi exercitationem quasi repellendus optio ducimus cupiditate officia culpa perferendis. Eum recusandae impedit suscipit tenetur, modi aspernatur.
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam pariatur temporibus illum eum sequi deserunt aut. Laborum provident alias maiores neque nihil harum esse voluptates, voluptate fuga, repudiandae rerum nostrum.
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime soluta ipsum totam pariatur eveniet! Ipsum, rerum iusto dolores reprehenderit dignissimos incidunt esse mollitia quaerat doloremque! Quasi quo est veritatis optio.
+                </p>
+            </div>
+    </div>
+
+</div>
+    
+</body>
+</html>
